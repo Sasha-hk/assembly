12fec9ef09497ee7ef82b125ca49ac8f994144af
Petryk Oleksandr <sachapos50@gmail.com>
2022-06-15 00:16:06 +0300

    feat: rename folders

--- base/hello-world/middle/main.asm
+++ base/hello-world/middle/main.asm
@@ -1,16 +1,55 @@
-section .text
 global _start
-_start:
-  mov eax, 0x4
-  mov ebx, 1
-  mov ecx, msg
-  mov edx, len
-  int 0x80
-
-  mov eax, 0x1
-  mov ebx, 0
-  int 0x80
 
 section .data
-msg db 'Hello world', 0xA
-len equ $ - msg
+msg db "Hello world", endl, 0
+endl equ 0xA
+
+section .text
+_start:
+  mov rax, msg
+  call print_string
+  call print_string
+  call exit
+
+; | input:
+; rax = string
+print_string:
+  push rax
+  push rbx
+  push rcx
+  push rdx
+  mov rcx, rax
+  call length_string
+  mov rdx, rax
+  mov rax, 4
+  mov rbx, 1
+  int 0x80
+  pop rax
+  pop rbx
+  pop rcx
+  pop rdx
+  ret
+
+; | input:
+; rex = string
+; | outpus:
+; rax = length
+length_string:
+  push rdx
+  xor rdx, rdx
+  .next_iter:
+    cmp [rax + rdx], byte 0
+    je .close
+    inc rdx
+    jmp .next_iter
+  .close:
+    mov rax, rdx
+    pop rdx
+    ret
+  ret
+
+exit:
+  mov rax, 1
+  mov rbx, 1
+  int 0x80
+
