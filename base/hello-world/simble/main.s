21c1eddef0a43ed539751579e4c88d5c1029d750
Petryk Oleksandr <sachapos50@gmail.com>
2022-06-16 21:38:41 +0300

    fix: code style

--- base/hello-world/simble/main.s
+++ base/hello-world/simble/main.s
@@ -1,16 +1,17 @@
-section .text
 global _start
+
+section .data
+  msg db "Hello world", 0xA, 0
+  len equ $ - msg
+
+section .text
 _start:
   mov eax, 0x4
-  mov ebx, 1
+  mov ebx, 0x1
   mov ecx, msg
   mov edx, len
   int 0x80
 
   mov eax, 0x1
-  mov ebx, 0
+  mov ebx, 0x0
   int 0x80
-
-section .data
-msg db 'Hello world', 0xA
-len equ $ - msg
